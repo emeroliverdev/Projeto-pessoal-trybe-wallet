3b51c704ca8cd6f217eff77e07bd99001d51b205
Emerson Oliveira <emerson.contato@gmail.com>
2021-11-22 11:24:51 -0300

    Requisitos 1 e 2

---
+++ src/actions/fecthAPI.js
@@ -0,0 +1,7 @@
+const fetchCurrency = () => (
+  fetch('https://economia.awesomeapi.com.br/json/all')
+    .then((response) => response.json()
+      .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))))
+);
+
+export default fetchCurrency;
